124289962963ea45a2a0d239ae126883f5eb5dcd
nazam9519 <nazam9519@gmail.com>
2015-11-17 14:40:16 -0500

    Testing WCF and Soap

---
+++ Team_Compostable/t1.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/GreenLiving.Master" AutoEventWireup="true" CodeBehind="t1.aspx.cs" Inherits="Team_Compostable.t1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="mainHomeContent" runat="server">
+    <asp:Label id="yo" runat="server"></asp:Label>
+</asp:Content>
---
+++ Team_Compostable/t1.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Team_Compostable.retrive;
+
+namespace Team_Compostable
+{
+    public partial class t1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+            Service1 retrive = new Service1();
+            yo.Text = retrive.fnlName("yo","hi");
+        }
+    }
+}
