43e76c4a8f51c32b07df7670543f4e862a7779a1
bvx5009 <baxadirxudayberdiev@gmail.com>
2015-11-17 08:36:51 -0500

    user page added

--- Team_Compostable/GreenLiving.Master.designer.cs
+++ Team_Compostable/GreenLiving.Master.designer.cs
@@ -137,5 +137,23 @@ namespace Team_Compostable {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder contactContent;
+        
+        /// <summary>
+        /// mainUserWelcome control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder mainUserWelcome;
+        
+        /// <summary>
+        /// mainUserContent control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder mainUserContent;
     }
 }
